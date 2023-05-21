1319cd552e9616351300b53dbbc78c2d0623b7ab
krajeet35 <krajeet35@gmail.com>
2023-05-21 19:17:20 +0530

    crime1

---
+++ Crime_Information_Management_System/src/main/java/com/ajeet/CrimeInformationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ajeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeInformationManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimeInformationManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Crime_Information_Management_System/src/test/java/com/ajeet/CrimeInformationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeInformationManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
